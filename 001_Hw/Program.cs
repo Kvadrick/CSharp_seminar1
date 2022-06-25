f0e3b87834afe0b31747326f9634cf37eac76324
kvadrick <nmsmet@ya.ru>
2022-06-25 14:40:28 +0300

    Add folder 001_Hw

---
+++ 001_Hw/Program.cs
@@ -0,0 +1,24 @@
+﻿int numberA = new Random().Next(1, 100);
+int numberB = new Random().Next(1, 100);
+
+Console.WriteLine(numberA);
+Console.WriteLine(numberB);
+
+int Max = numberA;
+int min = numberB;
+
+if(numberA > numberB)
+{
+    Max = numberA;
+    min = numberB;
+}
+else
+{
+    Max = numberB;
+    min = numberA;
+}
+Console.Write("Max = ");
+Console.WriteLine(Max);
+
+Console.Write("min = ");
+Console.WriteLine(min);
